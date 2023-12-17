84ff2d1c931ab431dc437ea41d8914035faeb099
murilo <murilo.livorato@jhe.com.br>
2023-12-17 14:22:42 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <form @submit.prevent="storeForm" >
+      <submit-btn :processloading="processingForm"
+                  faiconposition="left"
+                  stylebutton="btn_cl_left btn-green-md1"
+                  textbutton="Search" ></submit-btn>
+    </form>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+import SubmitBtn from '@/components/SubmitBtn.vue'
+const processingForm = ref(false)
+const storeForm = () => {
+  processingForm.value = true
+  // VERIFY ERRORS
+  // PROCESSING FALSE
+  const wait = setTimeout(() => {
+    clearTimeout(wait)
+    processingForm.value = false
+  }, 2000)
+}
+</script>
+<style scoped>
+</style>
